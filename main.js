e9124e9ccfbc8052c6e7de313c3934f7ea5bdd9b
golopinok <golopinok01@gmail.com>
2025-12-12 22:39:17 +0000

    my physics calculator

---
+++ main.js
@@ -0,0 +1,82 @@
+var select = "f";
+function start() {
+    var input1 = document.getElementById("input1").value;
+    var input2 = document.getElementById("input2").value;
+    switch(select) {
+        case "f":
+            document.getElementById("inner").innerHTML = "F="+(input1*input2)+"H";
+            break;
+        case "v":
+            document.getElementById("inner").innerHTML = "V="+(input1/input2)+"m/s";
+            break;
+        case "s":
+            document.getElementById("inner").innerHTML = "S="+(input1*input2)+"m";
+            break;
+        case "t":
+            document.getElementById("inner").innerHTML = "t="+(input1/input2)+"s";
+            break;
+        case "vo":
+            document.getElementById("inner").innerHTML = "V="+(input1*input2)+"cm<sup>3</sup>";
+            break;
+        case "k":
+            document.getElementById("inner").innerHTML = "k="+(input1/input2)+"H/m";
+            break;
+        case "fg":
+            document.getElementById("inner").innerHTML = "F="+(input1*input2)+"H";
+            break;
+    }
+}
+
+function check_select(input) {
+    switch(input) {
+        case "f":
+            document.getElementById("formula").innerHTML = "F=mg";
+            document.getElementById("fnum").innerHTML = "m=";
+            document.getElementById("snum").innerHTML = "g=";
+            document.getElementById("fnum-mer").innerHTML = "kg";
+            document.getElementById("snum-mer").innerHTML = "m/s<sup>2</sup>";
+            break;
+        case "v":
+            document.getElementById("formula").innerHTML = "V=S/t";
+            document.getElementById("fnum").innerHTML = "S=";
+            document.getElementById("snum").innerHTML = "t=";
+            document.getElementById("fnum-mer").innerHTML = "m";
+            document.getElementById("snum-mer").innerHTML = "s";
+            break;
+        case "s":
+            document.getElementById("formula").innerHTML = "S=Vt";
+            document.getElementById("fnum").innerHTML = "V=";
+            document.getElementById("snum").innerHTML = "t=";
+            document.getElementById("fnum-mer").innerHTML = "m/s";
+            document.getElementById("snum-mer").innerHTML = "s";
+            break;
+        case "t":
+            document.getElementById("formula").innerHTML = "t=S/V";
+            document.getElementById("fnum").innerHTML = "S=";
+            document.getElementById("snum").innerHTML = "V=";
+            document.getElementById("fnum-mer").innerHTML = "m";
+            document.getElementById("snum-mer").innerHTML = "m/s";
+            break;
+        case "vo":
+            document.getElementById("formula").innerHTML = "V=Sh";
+            document.getElementById("fnum").innerHTML = "S=";
+            document.getElementById("snum").innerHTML = "h=";
+            document.getElementById("fnum-mer").innerHTML = "cm<sup>2</sup>";
+            document.getElementById("snum-mer").innerHTML = "cm";
+            break;
+        case "k":
+            document.getElementById("formula").innerHTML = "k=F/l";
+            document.getElementById("fnum").innerHTML = "F=";
+            document.getElementById("snum").innerHTML = "l=";
+            document.getElementById("fnum-mer").innerHTML = "H/m";
+            document.getElementById("snum-mer").innerHTML = "m";
+            break;
+        case "fg":
+            document.getElementById("formula").innerHTML = "F=kl";
+            document.getElementById("fnum").innerHTML = "k=";
+            document.getElementById("snum").innerHTML = "l=";
+            document.getElementById("snum-mer").innerHTML = "m";
+            break;
+    }
+    select = input;
+}
